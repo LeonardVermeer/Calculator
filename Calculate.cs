4dadf88665525bf9c023043e2b7ffba13ae0db94
Leonard <lgvermeer@gmail.com>
2023-11-15 17:43:29 +0200

    Initial commit

---
+++ Calculate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Calculator {
+    internal class Calculate {
+        List <String> numbers = new List <String> ();
+        List <String> equation = new List <String> ();
+
+        public void InsertNumber(char Num) {
+            numbers.Add(Convert.ToString(Num));
+        }
+
+        private void InsertNumberIntoEquation() {
+            string number = "";
+            foreach (string num in numbers) {
+                number += num;
+            }
+            equation.Add(number);
+        }
+
+        public void InsertOperator(string Operator) {
+            InsertNumberIntoEquation();
+            equation.Add(Operator);
+
+            numbers = new List<string> ();
+        }
+
+        public string Calculation() {
+            InsertNumberIntoEquation();
+
+            //loop through uneaven indexes and if a / or a x is found, calculate
+            //then loop through uneaven indexes and calculate
+
+            //Start at an operator index 0 will always be a number
+            for (int i = 1; i <= equation.Count - 1; i+= 2) { //equation.Count - 1 because the last one will always be a number
+            //All symbols will be in uneaven indexes
+                if (equation[i] == "/") {
+                    float.TryParse(equation[i - 1], out float num1);
+                    float.TryParse(equation[i + 1], out float num2);
+
+                    equation[i - 1] = Convert.ToString(num1 / num2);
+                    equation.RemoveRange(i, 2);
+                    i -= 2;
+                } else if (equation[i] == "x") {
+                    float.TryParse(equation[i - 1], out float num1);
+                    float.TryParse(equation[i + 1], out float num2);
+
+                    equation[i - 1] = Convert.ToString(num1 * num2);
+                    equation.RemoveRange(i, 2);
+                    i -= 2;
+                }
+                
+            }
+
+            //All / and x calculations are done
+            //Now only + and - are left
+
+            while (equation.Count > 2) { 
+                if (equation[1] == "+") {
+                    float.TryParse(equation[0], out float num1);
+                    float.TryParse(equation[2], out float num2);
+
+                    equation[0] = Convert.ToString(num1 + num2);
+                    equation.RemoveRange(1, 2);
+                } else if (equation[1] == "-") {
+                    float.TryParse(equation[0], out float num1);
+                    float.TryParse(equation[2], out float num2);
+
+                    equation[0] = Convert.ToString(num1 - num2);
+                    equation.RemoveRange(1, 2);
+                }
+            }
+            return equation[0].ToString();
+        }
+
+        public void Clear() {
+            numbers = new List<String>();
+            equation = new List<String>();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        Calculate calculate = new Calculate();
+        private void btnOne_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('1');
+            rtbAnswer.Text = rtbAnswer.Text + '1';
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('2');
+            rtbAnswer.Text = rtbAnswer.Text + '2';
+        }
+
+        private void btnThree_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('3');
+            rtbAnswer.Text = rtbAnswer.Text + '3';
+        }
+
+        private void btnFour_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('4');
+            rtbAnswer.Text = rtbAnswer.Text + '4';
+        }
+
+        private void btnFive_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('5');
+            rtbAnswer.Text = rtbAnswer.Text + '5';
+        }
+
+        private void btnSix_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('6');
+            rtbAnswer.Text = rtbAnswer.Text + '6';
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('7');
+            rtbAnswer.Text = rtbAnswer.Text + '7';
+        }
+
+        private void btnEight_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('8');
+            rtbAnswer.Text = rtbAnswer.Text + '8';
+        }
+
+        private void btnNine_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('9');
+            rtbAnswer.Text = rtbAnswer.Text + '9';
+        }
+
+        private void btnZero_Click(object sender, EventArgs e) {
+            calculate.InsertNumber('0');
+            rtbAnswer.Text = rtbAnswer.Text + '0';
+        }
+
+        private void btnDecimal_Click(object sender, EventArgs e) {
+            calculate.InsertNumber(',');
+            rtbAnswer.Text = rtbAnswer.Text + ',';
+        }
+
+        private void button13_Click(object sender, EventArgs e) {
+            calculate.InsertOperator("+");
+            rtbAnswer.Text = rtbAnswer.Text + " + ";
+        }
+
+        private void button15_Click(object sender, EventArgs e) {
+            calculate.InsertOperator("-");
+            rtbAnswer.Text = rtbAnswer.Text +  " - ";
+        }
+
+        private void button14_Click(object sender, EventArgs e) {
+            calculate.InsertOperator("x");
+            rtbAnswer.Text = rtbAnswer.Text + " x ";
+        }
+
+        private void button16_Click(object sender, EventArgs e) {
+            calculate.InsertOperator("/");
+            rtbAnswer.Text = rtbAnswer.Text + " / ";
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e) {
+            rtbAnswer.Text = rtbAnswer.Text + " = " + Convert.ToString(calculate.Calculation());
+        }
+
+        private void btnClear_Click(object sender, EventArgs e) {
+            calculate.Clear();
+            rtbAnswer.Clear();
+        }
+    }
+}
