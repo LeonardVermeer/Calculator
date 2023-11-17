7e0e829418d4050dcac7c1d12cae91847924f776
LeonardVermeer <lgvermeer@gmail.com>
2023-11-17 19:04:52 +0200

    The negative is a part of the number now

--- Calculate.cs
+++ Calculate.cs
@@ -15,16 +15,27 @@ namespace Calculator {
             numbers.Add(Convert.ToString(Num));
         }
 
-        private void InsertNumberIntoEquation() {
+        private void InsertNumberIntoEquation(bool isNegative = false) {
             string number = "";
+
             foreach (string num in numbers) {
                 number += num;
             }
+
+            if (isNegative) {
+               number.Insert(0, "-");
+            }
+    
             equation.Add(number);
         }
 
         public void InsertOperator(string Operator) {
-            InsertNumberIntoEquation();
+            if (Operator == "-") {
+                InsertNumberIntoEquation(true);
+            } else {
+                InsertNumberIntoEquation();
+            }
+
             equation.Add(Operator);
 
             numbers = new List<string> ();
@@ -37,20 +48,28 @@ namespace Calculator {
             //and that second function calls the original function,
             //it creates a new instance of the stack frame for each function call.
             //Each stack frame maintains its own set of local variables, including parameters,
-            //and has its own execution context."
+            //and has its own execution context."            
 
-            //PEDMAS
-            //One funciton for each of them
-
-            for (int i = 1; i <= equation.Count - 1; i++) { //equation.Count - 1 because the last one will always be a number
+            for (int i = 0; i <= equation.Count - 1; i++) { //equation.Count - 1 because the last one will always be a number or can be ignored
                 if (equation[i] == "/") {
                     float.TryParse(equation[i - 1], out float num1);
-                    float.TryParse(equation[i + 1], out float num2);
+                    if (equation[i - 2] == "-") { num1 *= -1; }
+                    //Instead of manually trying to work with negatives
+                    //add the negative to the number and continue as normal
+                    //The list would then look like
+                    //3 -4 + 2 -5 / -2 + 3 / -6
+                    //0  1 2 3 4  5  6 7 8 9 10
 
-                    equation[i - 1] = Convert.ToString(num1 / num2);
+                    //float.TryParse(equation[i + 1], out float num2);
+                    if (equation[i + 1] == "-") { 
+                        float.TryParse(equation[i + 2], out float num2);
+                    }
+
+                    equation[i - 1] = Convert.ToString(num1 / num1); //num1 / num2 check (just to remove error)
                     equation.RemoveRange(i, 2);
                     i -= 2;
-                } else if (equation[i] == "x") {
+                }
+                else if (equation[i] == "x") {
                     float.TryParse(equation[i - 1], out float num1);
                     float.TryParse(equation[i + 1], out float num2);
 
@@ -58,28 +77,77 @@ namespace Calculator {
                     equation.RemoveRange(i, 2);
                     i -= 2;
                 }
-                
+
             }
 
-            //All / and x calculations are done
-            //Now only + and - are left
+            ////All / and x calculations are done
+            ////Now only + and - are left
 
-            while (equation.Count > 2) { 
-                if (equation[1] == "+") {
-                    float.TryParse(equation[0], out float num1);
-                    float.TryParse(equation[2], out float num2);
+            //while (equation.Count > 2) { 
+            //    if (equation[1] == "+") {
+            //        float.TryParse(equation[0], out float num1);
+            //        float.TryParse(equation[2], out float num2);
 
-                    equation[0] = Convert.ToString(num1 + num2);
-                    equation.RemoveRange(1, 2);
-                } else if (equation[1] == "-") {
-                    float.TryParse(equation[0], out float num1);
-                    float.TryParse(equation[2], out float num2);
+            //        equation[0] = Convert.ToString(num1 + num2);
+            //        equation.RemoveRange(1, 2);
+            //    } else if (equation[1] == "-") {
+            //        float.TryParse(equation[0], out float num1);
+            //        float.TryParse(equation[2], out float num2);
 
-                    equation[0] = Convert.ToString(num1 - num2);
-                    equation.RemoveRange(1, 2);
+            //        equation[0] = Convert.ToString(num1 - num2);
+            //        equation.RemoveRange(1, 2);
+            //    }
+            //}
+            //return equation[0].ToString();
+
+            return "";
+        }
+
+        public string Divide(string num1, string num2) {
+            float.TryParse(num1, out float num1f);
+            float.TryParse(num2, out float num2f);
+
+            return Convert.ToString(num1f / num2f);
+        }
+        
+        public string Multiply(string num1, string num2) {
+            float.TryParse(num1, out float num1f);
+            float.TryParse(num2, out float num2f);
+
+            return Convert.ToString(num1f * num2f);
+        }
+        
+        public string Add(string num1, string num2) {
+            float.TryParse(num1, out float num1f);
+            float.TryParse(num2, out float num2f);
+
+            return Convert.ToString(num1f + num2f);
+        }
+        
+        public string Subtract(string num1, string num2) {
+            float.TryParse(num1, out float num1f);
+            float.TryParse(num2, out float num2f);
+
+            return Convert.ToString(num1f - num2f);
+        }
+
+        public bool IsSymbolTheLastCharacter(string symbol) {
+            //Check if some symbol isn't before another
+            //Or calculate them regardless (- x - = +)
+            //If - x - = + then - / - = +
+            if (numbers.Count > 0) {
+                if (numbers[numbers.Count - 1] == symbol) {
+                    return true; //The symbol is already in the equation
                 }
             }
-            return equation[0].ToString();
+
+            if (equation.Count > 0) {
+                if (equation[equation.Count - 1] == symbol) {
+                    return true;
+                }
+            }            
+
+            return false;
         }
 
         public void Clear() {
--- Form1.cs
+++ Form1.cs
@@ -17,81 +17,94 @@ namespace Calculator {
         Calculate calculate = new Calculate();
         private void btnOne_Click(object sender, EventArgs e) {
             calculate.InsertNumber('1');
-            rtbAnswer.Text = rtbAnswer.Text + '1';
+            rtbAnswer.Text += '1';
         }
 
         private void btnTwo_Click(object sender, EventArgs e) {
             calculate.InsertNumber('2');
-            rtbAnswer.Text = rtbAnswer.Text + '2';
+            rtbAnswer.Text += '2';
         }
 
         private void btnThree_Click(object sender, EventArgs e) {
             calculate.InsertNumber('3');
-            rtbAnswer.Text = rtbAnswer.Text + '3';
+            rtbAnswer.Text += '3';
         }
 
         private void btnFour_Click(object sender, EventArgs e) {
             calculate.InsertNumber('4');
-            rtbAnswer.Text = rtbAnswer.Text + '4';
+            rtbAnswer.Text += '4';
         }
 
         private void btnFive_Click(object sender, EventArgs e) {
             calculate.InsertNumber('5');
-            rtbAnswer.Text = rtbAnswer.Text + '5';
+            rtbAnswer.Text += '5';
         }
 
         private void btnSix_Click(object sender, EventArgs e) {
             calculate.InsertNumber('6');
-            rtbAnswer.Text = rtbAnswer.Text + '6';
+            rtbAnswer.Text += '6';
         }
 
         private void btnSeven_Click(object sender, EventArgs e) {
             calculate.InsertNumber('7');
-            rtbAnswer.Text = rtbAnswer.Text + '7';
+            rtbAnswer.Text += '7';
         }
 
         private void btnEight_Click(object sender, EventArgs e) {
             calculate.InsertNumber('8');
-            rtbAnswer.Text = rtbAnswer.Text + '8';
+            rtbAnswer.Text += '8';
         }
 
         private void btnNine_Click(object sender, EventArgs e) {
             calculate.InsertNumber('9');
-            rtbAnswer.Text = rtbAnswer.Text + '9';
+            rtbAnswer.Text += '9';
         }
 
         private void btnZero_Click(object sender, EventArgs e) {
             calculate.InsertNumber('0');
-            rtbAnswer.Text = rtbAnswer.Text + '0';
+            rtbAnswer.Text += '0';
         }
 
         private void btnDecimal_Click(object sender, EventArgs e) {
-            calculate.InsertNumber(',');
-            rtbAnswer.Text = rtbAnswer.Text + ',';
+            if (!calculate.IsSymbolTheLastCharacter(",")) {
+                calculate.InsertNumber(',');
+                rtbAnswer.Text += ',';
+            } 
         }
 
         private void button13_Click(object sender, EventArgs e) {
-            calculate.InsertOperator("+");
-            rtbAnswer.Text = rtbAnswer.Text + " + ";
+            if (!calculate.IsSymbolTheLastCharacter("+")) {
+                calculate.InsertOperator("+");
+                rtbAnswer.Text += " + ";
+            }
         }
 
         private void button15_Click(object sender, EventArgs e) {
-            calculate.InsertOperator("-");
-            rtbAnswer.Text = rtbAnswer.Text +  " - ";
+            //Min man moeilikheid!
+            if (!calculate.IsSymbolTheLastCharacter("-")) {
+                calculate.InsertOperator("-");
+                rtbAnswer.Text += " - ";
+            }
         }
 
         private void button14_Click(object sender, EventArgs e) {
-            calculate.InsertOperator("x");
-            rtbAnswer.Text = rtbAnswer.Text + " x ";
+            if (!calculate.IsSymbolTheLastCharacter("x")) {
+                calculate.InsertOperator("x");
+                rtbAnswer.Text += " x ";
+            }
         }
 
         private void button16_Click(object sender, EventArgs e) {
-            calculate.InsertOperator("/");
-            rtbAnswer.Text = rtbAnswer.Text + " / ";
+            if (!calculate.IsSymbolTheLastCharacter("/")) {
+                calculate.InsertOperator("/");
+                rtbAnswer.Text += " / ";
+            }
         }
 
         private void btnEqual_Click(object sender, EventArgs e) {
-            rtbAnswer.Text = rtbAnswer.Text + " = " + Convert.ToString(calculate.Calculation());
+            if (!calculate.IsSymbolTheLastCharacter("=")) {
+                rtbAnswer.Text += " = " + Convert.ToString(calculate.Calculation());
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e) {
