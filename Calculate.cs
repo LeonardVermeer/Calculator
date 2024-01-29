d2319e2aaf29268491f421dc9ffaa4d48eb61b46
Leonard Gerrit Vermeer <577309@student.belgiumcampus.ac.za>
2024-01-29 20:16:13 +0200

    Everything works! Now just for added functionality.

--- Calculate.cs
+++ Calculate.cs
@@ -39,29 +39,14 @@ namespace Calculator {
         }
 
         public string Calculation() {
-            //I have to use loops and call functions
-            //"In C#, when a function calls another function,
-            //and that second function calls the original function,
-            //it creates a new instance of the stack frame for each function call.
-            //Each stack frame maintains its own set of local variables, including parameters,
-            //and has its own execution context."            
-
-            for (int i = 0; i <= equation.Count - 1; i++) { //equation.Count - 1 because the last one will always be a number or can be ignored
+            for (int i = 0; i <= equation.Count - 2; i++) { //equation.Count - 2 because the last one will always be the = sign and the only digit which is the answer
                 if (equation[i] == "/") { //PEDMAS
                     float.TryParse(equation[i - 1], out float num1);
-                    
-                    //Instead of manually trying to work with negatives
-                    //add the negative to the number and continue as normal
-                    //The list would then look like
-                    //3 -4 + 2 -5 / -2 + 3 / -6
-                    //0  1 2 3 4  5  6 7 8 9 10
-
                     float.TryParse(equation[i + 1], out float num2);
-                    
 
                     equation[i - 1] = Convert.ToString(num1 / num2);
                     equation.RemoveRange(i, 2);
-                    i -= 2; //Adjust for the removed characters
+                    i -= 1; //Adjust for the removed characters
                 }
                 else if (equation[i] == "x") {
                     float.TryParse(equation[i - 1], out float num1);
@@ -69,52 +54,39 @@ namespace Calculator {
 
                     equation[i - 1] = Convert.ToString(num1 * num2);
                     equation.RemoveRange(i, 2);
-                    i -= 2;
+                    i -= 1;
                 }
 
             }
 
-            ////All / and x calculations are done
-            ////Now only + and - are left
+            //All / and x calculations are done
+            //Now only + and - are left
 
-            //while (equation.Count > 2) {
-            //    if (equation[1] == "+") {
-            //        float.TryParse(equation[0], out float num1);
-            //        float.TryParse(equation[2], out float num2);
-
-            //        equation[0] = Convert.ToString(num1 + num2);
-            //        equation.RemoveRange(1, 2);
-            //    }
-            //    else if (equation[1] == "-") {
-            //        float.TryParse(equation[0], out float num1);
-            //        float.TryParse(equation[2], out float num2);
-
-            //        equation[0] = Convert.ToString(num1 - num2);
-            //        equation.RemoveRange(1, 2);
-            //    }
-            //}
-
-            for (int i = 0; i  < equation.Count - 1; i++) {
+            for (int i = 0; i  < equation.Count - 2; i++) {
                 if (equation[i] == "+") {
                     float.TryParse(equation[i - 1], out float num1);
                     float.TryParse(equation[i + 1], out float num2);
 
                     equation[i - 1] = Convert.ToString(num1 + num2);
                     equation.RemoveRange(i, 2);
-                    i -= 2; //Adjust for the removed characters
-                } else if (equation.Count == 3) {
-                    float.TryParse(equation[0], out float num1);
-                    float.TryParse(equation[1], out float num2);
+                    i -= 1; //Adjust for the removed characters
+                } 
+            }
 
-                    equation[0] = Convert.ToString(num1 + num2);//It doesn't matter which number has the negative it will be accounted for
-                    return equation[0].ToString();
+            //Now only the negative numbers remain and we can just add them together
+
+            for (int i = 0; i <= equation.Count - 2; i++) {
+                if (equation.Count >= 2) { //If there's anything left it's a negative and can be added
+                    float.TryParse(equation[i], out float num1);
+                    float.TryParse(equation[i + 1], out float num2);
+
+                    equation[i] = Convert.ToString(num1 + num2); //It doesn't matter which number has the negative it will be accounted for
+                    equation.RemoveRange(i + 1, 1);
+                    i -= 1;
                 }
             }
 
             return equation[0].ToString();
-
-
-            //return "";
         }
 
         //public bool IsLastCharacterASymbol() {
